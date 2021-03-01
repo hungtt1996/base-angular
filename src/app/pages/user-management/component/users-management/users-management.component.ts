062c93401f27f8d4de97a16ca98144490ee25b22
hungtt <trantienhung.bk@gmail.com>
2021-03-01 22:20:12 +0700

    filter table

--- src/app/pages/user-management/component/users-management/users-management.component.ts
+++ src/app/pages/user-management/component/users-management/users-management.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, Validators} from '@angular/forms';
 import {BehaviorSubject} from 'rxjs';
 import {Router} from '@angular/router';
 
@@ -10,10 +10,24 @@ import {Router} from '@angular/router';
 })
 export class UsersManagementComponent implements OnInit {
     users$: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
+    dropdownActive$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    position$: BehaviorSubject<{ xPosition: number, yPosition: number }> = new BehaviorSubject<{ xPosition: number, yPosition: number }>(null);
+
+    //filter
+    status = [
+        {code: 'PENDING', value: 'Pending'},
+        {code: 'REJECTED', value: 'Rejected'},
+        {code: 'APPROVED', value: 'Approved'},
+        {code: 'CANCEL', value: 'Cancel'}
+    ];
 
     searchForm = this.fb.group({
         fullname: [null, [Validators.required]],
         username: [null, [Validators.required]],
+    });
+
+    statusForm = this.fb.group({
+        statusArray: this.fb.array([])
     });
 
     constructor(
@@ -49,5 +63,49 @@ export class UsersManagementComponent implements OnInit {
 
     checkAllItems(event): void {
         console.log('Check all items >>>', event);
+    }
+
+    onTriggerMenu(event: MouseEvent, property: string): void {
+        console.log('>>>> onclick ', event);
+        if (property == 'role_code') {
+            this.dropdownActive$.next(true);
+            return;
+        }
+        this.dropdownActive$.next(false);
+    }
+
+    onCheckboxChange(event): void {
+        console.log(event.target.value);
+        const statusArray: FormArray = this.statusForm.get('statusArray') as FormArray;
+        if (event.target.checked && !statusArray.value.includes(event.target.value)) {
+            statusArray.push(new FormControl(event.target.value));
+        } else if (!event.target.checked) {
+            let i: number = 0;
+            statusArray.controls.forEach((item: FormControl) => {
+                if (item.value == event.target.value) {
+                    statusArray.removeAt(i);
+                    return;
+                }
+                i++;
+            });
+        }
+
+        console.log(statusArray.value);
+
+    }
+
+    onShowPstatus(): void {
+        setTimeout(() => {
+            const statusArray: FormArray = this.statusForm.get('statusArray') as FormArray;
+            console.log('on show pop over', statusArray);
+            statusArray.value.forEach(status => {
+                const ele = document.getElementById(status) as HTMLInputElement;
+                ele.checked = true;
+            });
+        }, 50);
+    }
+
+    filterByStatus(source: string[]): void {
+
     }
 }
